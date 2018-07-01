a0cddbee7f4a5636911fa8de376b7fcdf4c788ce
Danxue Huang <36866155+dh0072@users.noreply.github.com>
2018-07-01 17:16:21 -0500

    Update reports.py
    Initial implementation of markdown

--- reports.py
+++ reports.py
@@ -4,6 +4,7 @@ import markdown
 def gen_overall_progress(overall_progress, md = markdown.markdown()):
     md.gen_heading('Overall Progress', 1)
     md.gen_line('')
+
     md.gen_table(overall_progress.keys(), [overall_progress.values()])
     md.gen_line('')
     return md.content
@@ -34,17 +35,62 @@ def gen_tickets_stats_by_category(by_category, md = markdown.markdown()):
 
 
 def gen_individual_tickets_info(tickets, md = markdown.markdown()):
-    for ticket_id in tickets:
-        # md.gen_heading(ticket_id, 2)
-        # md.gen_line('')
+    md.gen_heading('Tickets', 1)
+    md.gen_line('')
 
+    for ticket_id in tickets:
         # Generate markdown for ticket meta data
         ticket_meta = tickets[ticket_id]['meta']
-        # md.gen_heading('meta', 3)
-        # md.gen_line('')
 
-        # md.gen_table(ticket_meta.keys(), [ticket_meta.values()])
-        # md.gen_line('')
+        ticket_link = tickets[ticket_id].get('comment_attachment', {})\
+            .get('link', None)
+        if ticket_link:
+            md.gen_heading(md.gen_hyperlink(ticket_id, ticket_link), 2)
+            md.gen_line('')
 
-        # Generate markdown for ticket's comments and attachments
-        # comment_attachment = tickets[ticket_id]['comment_attachment']
+        md.gen_heading('meta', 3)
+        md.gen_line('')
+
+        description = ticket_meta.get('description', None)
+        summary = ticket_meta.get('summary', None)
+
+        ticket_meta.pop('description', None)
+        ticket_meta.pop('summary', None)
+
+        md.gen_table(header=ticket_meta.keys(), rows=[ticket_meta.values()])
+        md.gen_line('')
+
+        if description:
+            md.gen_bullet_point(md.gen_bold('description'))
+            md.gen_raw_text(description)
+            md.gen_line('')
+
+        if summary:
+            md.gen_bullet_point(md.gen_bold('summary'))
+            md.gen_raw_text(summary)
+            md.gen_line('')
+
+        # Generate markdown for ticket's comments or attachments
+        items = tickets[ticket_id]['comment_attachment']['items']
+        comments = items.get('comments', None)
+        attachments = items.get('attachments', None)
+
+        if len(comments) > 0:
+            comments_header = comments[0].keys()
+            comments_rows = []
+            for comment in comments:
+                comments_rows.append(list(comment.values()))
+            md.gen_heading('comments', 3)
+            md.gen_line('')
+            md.gen_table(comments_header, comments_rows)
+            md.gen_line('')
+
+        if len(attachments) > 0:
+            attachments_header = attachments[0].keys()
+            attachments_rows = []
+            for attachment in attachments:
+                attachments_rows.append(list(attachment.values()))
+            md.gen_heading('attachments', 3)
+            md.gen_line('')
+            md.gen_table(attachments_header, attachments_rows)
+            md.gen_line('')
